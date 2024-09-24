7081c659da55e6c8b8ab1429b2e49c4a44116635
Priyanshu Singh <152967318+Priyanshu4585@users.noreply.github.com>
2024-09-24 21:04:20 +0530

    Add files via upload

---
+++ javaScript/DOM model/Selectors JS/selectors.js
@@ -0,0 +1,25 @@
+// selectors
+
+document.firstElementChild;
+document.lastElementChild;
+document.querySelector("");
+document.querySelectorAll("");
+document.getElementsByTagName("");
+document.getElementsByClassName("");
+document.getElementById("");
+
+// changing properties
+
+style.color ="";
+style.backgroundColor="";
+style.fontSize="";
+innerHTML="";
+textContent="";
+
+
+
+// class list use
+
+classList.add("");
+classList.remove("");
+classList.toggle("");
