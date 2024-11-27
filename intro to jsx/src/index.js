f09afbf8ed598014f291a776fd0a5c316103ce53
Priyanshu Singh <152967318+Priyanshu4585@users.noreply.github.com>
2024-11-27 18:13:48 +0530

    Add files via upload

---
+++ intro to jsx/src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>HELLO</h1>
+    <p>Hi there</p>
+  </div>,
+  document.getElementById("root")
+);
