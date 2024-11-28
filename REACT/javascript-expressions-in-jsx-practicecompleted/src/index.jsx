dbbc917fa645babc67c98caf4ee1cd19c5c096c7
Priyanshu Singh <152967318+Priyanshu4585@users.noreply.github.com>
2024-11-29 00:46:13 +0530

    Update index.jsx

--- REACT/javascript-expressions-in-jsx-practicecompleted/src/index.jsx
+++ REACT/javascript-expressions-in-jsx-practicecompleted/src/index.jsx
@@ -13,8 +13,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
