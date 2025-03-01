534046397f3c7b7ca4d738a99d349889f35749d2
Priyanshu Singh <152967318+Priyanshu4585@users.noreply.github.com>
2025-03-01 22:00:56 +0530

    Add files via upload

---
+++ REACT/keeper app part 3/src/components/CreateArea.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+function CreateArea(props) {
+  const [note, setNote] = React.useState({
+    title: "",
+    content: "",
+  });
+
+  function handleChange(event) {
+    const { name, value } = event.target;
+
+    setNote((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  }
+
+  function submitNote(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
+  return (
+    <div>
+      <form>
+        <input
+          onChange={handleChange}
+          value={note.title}
+          name="title"
+          placeholder="Title"
+        />
+        <textarea
+          value={note.content}
+          onChange={handleChange}
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+        />
+        <button onClick={submitNote}>Add</button>
+      </form>
+    </div>
+  );
+}
+
+export default CreateArea;
